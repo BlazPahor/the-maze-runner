880ff324c3d5e113e22b380e92960e001ee568db
BlazPahor <97471923+BlazPahor@users.noreply.github.com>
2022-03-02 19:05:20 +0100

    Add files via upload

---
+++ js/sweetaleart.js
@@ -0,0 +1,12 @@
+function sweet_aleart(){
+    Swal.fire({
+        toast: false,
+        icon: 'info',
+        title: 'This was made by Blaz Pahor',
+        showConfirmButton: true,
+        position: 'center',
+        confirmButtonColor: '#00A300',
+        iconColor: '#00A300',
+
+    });
+}
